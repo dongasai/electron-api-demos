ed3dfd5df550a6bb5d46938e9b2747ee3a84ebfe
dongasai <1514582970@qq.com>
2020-09-12 10:58:52 +0800

    测试 setAsDefaultProtocolClient

--- main-process/system/protocol-handler.js
+++ main-process/system/protocol-handler.js
@@ -1,14 +1,26 @@
 const {app, dialog} = require('electron')
 const path = require('path')
 
+
+
+let aa = 1616161;
 if (process.defaultApp) {
   if (process.argv.length >= 2) {
     app.setAsDefaultProtocolClient('electron-api-demos', process.execPath, [path.resolve(process.argv[1])])
+
   }
+
 } else {
-  app.setAsDefaultProtocolClient('electron-api-demos')
+  aa= app.setAsDefaultProtocolClient('electron-api-demos');
+
+
 }
 
 app.on('open-url', (event, url) => {
   dialog.showErrorBox('Welcome Back', `You arrived from: ${url}`)
 })
+app.on('ready', () => {
+  // console.log(arguments)
+  dialog.showErrorBox('defaultApp ',`You arrived `+process.defaultApp+aa);
+})
+
